c550786652e77ccd3979fdc94b4a6b4b70c99445
LucienXian <lucienxian97@gmail.com>
2019-02-16 21:33:20 +0800

    Application Listener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +13,19 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    //在容器启动完成时，继承 CommandLineRunner 接口的类的 run 方法会被自动执行（前提是这个类被Spring 管理）
+    @Bean
+    public DataLoader dataLoader() {
+        return new DataLoader();
+    }
+
+    @Slf4j
+    static class DataLoader implements CommandLineRunner {
+
+        @Override
+        public void run(String... args) throws Exception {
+            log.info(".....Loading data...");
+        }
+    }
 }
 
